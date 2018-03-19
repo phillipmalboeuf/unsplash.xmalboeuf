fa9d22b9eb519afc66038c68f5cb5c7d5e88b3b0
Phillip Malboeuf <phil@boeuf.coffee>
2018-03-19 14:21:46 -0400

    home page gif background

--- scripts/routes/home.tsx
+++ scripts/routes/home.tsx
@@ -6,23 +6,23 @@ import { Tag } from "../components/tag"
 interface Props {}
 
 export const Home: React.SFC<Props> = (props) => {
-  return <div className="hero">
-    <div className="hero__content">
+  return <div className="hero black_back" style={{backgroundImage: "url(https://montrealuploads.imgix.net/unsplash.gif?auto=format,compress&frame=all)"}}>
+    <div className="hero__content white">
       <h1>Unsplash <span className="thin">X Malboeuf</span></h1>
       <p>Hi Unsplash 🙀!<br/>I've set out to show how I'm your next full-stack developer. Your quest stops here!</p>
 
       <div className="grid grid--guttered">
         <div className="col col--6of12">
-          <p>First, I'd like to say that I'm significantly experienced with these:</p>
+          <p>First, I'd like to say that I'm significantly experienced with these tools and concepts:</p>
           <p>JavaScript<br/>{["React", "Express", "TypeScript", "Parcel", "Webpack"].map((tag, index)=> <Tag key={index} label={tag} />)}</p>
-          <p>Python<br/>{["Flask", "Celey", "Tornado"].map((tag, index)=> <Tag key={index} label={tag} />)}</p>
-          <p>Concepts<br/>{["Rest APIs", "SSR", "WebSockets", "Micro-services", "Ecommerce", "UX Research"].map((tag, index)=> <Tag key={index} label={tag} />)}</p>
+          <p>Python<br/>{["Flask", "Celery", "Tornado", ].map((tag, index)=> <Tag key={index} label={tag} />)}</p>
+          <p>Concepts<br/>{["Rest APIs", "SSR", "WebSockets", "Micro-services", "Ecommerce", "Conversational UI", "UX Research"].map((tag, index)=> <Tag key={index} label={tag} />)}</p>
           <p>Other techs<br/>{["Dokku", "MongoDB", "Imgix", "Algolia", "Stripe", "Shopify"].map((tag, index)=> <Tag key={index} label={tag} />)}</p>
         </div>
         <div className="col col--6of12">
           <p>And second, I've made this thing over the weekend, a small extension to your <a href="https://source.unsplash.com" className="underline" target="_blank">source.unsplash.com</a>:</p>
-          <p><Button to="/demo" label="See Demo" /></p>
-          <p><small className="grey">The <a href="https://github.com/phillipmalboeuf/unsplash.xmalboeuf" className="underline" target="_blank">source files are available</a>. Let me know what you think of it.</small></p>
+          <p><Button outline to="/demo" label="See Demo" /></p>
+          <p><small>The <a href="https://github.com/phillipmalboeuf/unsplash.xmalboeuf" className="underline" target="_blank">source files are available</a>. Let me know what you think of it.</small></p>
         </div>
       </div>
     </div>
