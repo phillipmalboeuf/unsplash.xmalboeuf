9e3beee1d4786495cae6bf2586c46cc9ee396f1a
Phillip Malboeuf <phil@boeuf.coffee>
2018-03-19 14:21:08 -0400

    Demo page introduction texts

--- scripts/routes/demo.tsx
+++ scripts/routes/demo.tsx
@@ -11,7 +11,26 @@ export const Demo: React.SFC<Props> = (props) => {
     <div className="hero__content">
       <Link className="underline" to="/">Back to welcome page</Link>
       <h1 className="h1--massive">Unsplash <span className="thin">Embed</span></h1>
-      <div className="padded padded--tight light_grey_back normal_bottom">📣 This demo is not intended to be a real product. I understand that there's plenty of things missing.</div>
+      <p>Theoretically, the Unsplash Embed would be useful for photographers or photography writers to easily expose photos with associated meta-information as well as "like" and "download" buttons.</p>
+      <div className="grid grid--guttered">
+        <div className="col col--6of12">
+          <h4>Built with</h4>
+          <ol>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>Unsplash API</li>
+          </ol>
+        </div>
+        <div className="col col--6of12">
+          <h4>Things to improve</h4>
+          <ol>
+            <li>Preact instead of React for the Embed's render</li>
+            <li>Use a non-rate-limited API call</li>
+            <li>Provide photo sizes parameters</li>
+          </ol>
+        </div>
+      </div>
+      <div className="padded padded--tight light_grey_back normal_bottom">📣 This demo is not intended to be a real, finished, or necessary product.</div>
       <EmbedBuilder />
     </div>
     <div className="hero__topright">
