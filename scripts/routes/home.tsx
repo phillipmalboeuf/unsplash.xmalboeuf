452fa5753eae70b54e310ee5bc0f70cae8b19e6c
Phillip Malboeuf <phil@boeuf.coffee>
2018-03-20 13:47:09 -0400

    X Phillip Malboeuf

--- scripts/routes/home.tsx
+++ scripts/routes/home.tsx
@@ -8,7 +8,7 @@ interface Props {}
 export const Home: React.SFC<Props> = (props) => {
   return <div className="hero black_back" style={{backgroundImage: "url(https://montrealuploads.imgix.net/unsplash.gif?auto=format,compress&frame=all)"}}>
     <div className="hero__content white">
-      <h1>Unsplash <span className="thin">X Malboeuf</span></h1>
+      <h1>Unsplash <span className="thin">X Phillip Malboeuf</span></h1>
       <p>Hi Unsplash 🙀!<br/>I've set out to show how I'm your next full-stack developer. Your quest stops here!</p>
 
       <div className="grid grid--guttered">
@@ -21,7 +21,7 @@ export const Home: React.SFC<Props> = (props) => {
         </div>
         <div className="col col--6of12">
           <p>And second, I've made this thing over the weekend, a small extension to your <a href="https://source.unsplash.com" className="underline" target="_blank">source.unsplash.com</a>:</p>
-          <p><Button outline to="/demo" label="See Demo" /></p>
+          <p><Button to="/demo" label="See Demo" /></p>
           <p><small>The <a href="https://github.com/phillipmalboeuf/unsplash.xmalboeuf" className="underline" target="_blank">source files are available</a>. Let me know what you think of it.</small></p>
         </div>
       </div>
