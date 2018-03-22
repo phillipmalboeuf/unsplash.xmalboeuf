43957f19be5812e7b32fe0617536f00513fe174d
Phillip Malboeuf <phil@boeuf.coffee>
2018-03-22 13:30:03 -0400

    some past work

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -20,7 +20,18 @@ export const Home: React.SFC<Props> = (props) => {
           <p>Other techs<br/>{["Dokku", "MongoDB", "Imgix", "Algolia", "Stripe", "Shopify"].map((tag, index)=> <Tag key={index} label={tag} />)}</p>
         </div>
         <div className="col col--6of12">
-          <p>And second, I've made this thing this week, a small extension to your <a href="https://source.unsplash.com" className="underline" target="_blank">source.unsplash.com</a>:</p>
+          <p>Second, these are some of my recent projects (some freelance/some personal):</p>
+          <ol className="small">
+            <li><a href="https://www.hotelsdauphin.ca/" target="_blank">www.hotelsdauphin.ca</a></li>
+            <li><a href="https://aubergebishop.ca/" target="_blank">aubergebishop.ca</a></li>
+            <li><a href="https://store.unsplash.com/" target="_blank">store.unsplash.com</a></li>
+            <li><a href="https://juneswimwear.com/" target="_blank">juneswimwear.com</a></li>
+            <li><a href="https://wisemenscare.com/" target="_blank">wisemenscare.com</a></li>
+            <li><a href="https://stdenis.co/" target="_blank">stdenis.co</a></li>
+            <li><a href="https://www.hubstudio.co/" target="_blank">www.hubstudio.co</a></li>
+            <li><a href="http://www.danielaandrade.com/" target="_blank">www.danielaandrade.com</a></li>
+          </ol>
+          <p>Finally, I've made this thing this week, a small extension to your <a href="https://source.unsplash.com" className="underline" target="_blank">source.unsplash.com</a>:</p>
           <p><Button to="/demo" label="See Demo" /></p>
           <p><small>The <a href="https://github.com/phillipmalboeuf/unsplash.xmalboeuf" className="underline" target="_blank">source files are available</a>. Let me know what you think of it.</small></p>
         </div>
