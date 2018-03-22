6925b5396e90f6eb998bd52087485d4818cbb880
Phillip Malboeuf <phil@boeuf.coffee>
2018-03-22 12:06:54 -0400

    embed styles polish

--- app/embed.tsx
+++ app/embed.tsx
@@ -47,17 +47,17 @@ export class Embed extends React.Component<Props, State> {
 
   render() {
     return this.state.photo && this.state.photo.urls
-      ? <div style={{border: "1px solid #f1f1f1", borderRadius: "4px", boxShadow: "0 2px 4px #f1f1f1", overflow: "hidden"}}>
-        <img src={this.state.photo.urls.regular} />
-        <div style={{fontSize: "15px", color: "#111", padding: "20px"}}>
+      ? <div style={{maxWidth: "666px", margin: "15px 0", border: "1px solid #f1f1f1", borderRadius: "4px", boxShadow: "0 2px 4px #f1f1f1", overflow: "hidden"}}>
+        <img style={{display: "block", width: "100%"}} src={this.state.photo.urls.regular} />
+        <div style={{fontSize: "15px", fontFamily: "-apple-system,San Francisco,Helvetica Neue,Helvetica,sans-serif", color: "#111", padding: "20px"}}>
           <div style={{display: "flex"}}>
             <span style={{flex: 1}}>
-              <a href={this.state.photo.user.links.self} target="_blank" style={{display: "inline-flex", alignItems: "center", marginRight: "10px"}}>
+              <a href={this.state.photo.user.links.self} target="_blank" style={{display: "inline-flex", alignItems: "center", color: "#999", textDecoration: "none", marginRight: "10px"}}>
                 <img style={{display: "block", borderRadius: "50%", marginRight: "10px"}} src={this.state.photo.user.profile_image.small} />
                 <span style={{color: "#111"}}>{this.state.photo.user.name}</span>
               </a>
             </span>
-            {this.props.show_like_button && <a style={{display: "flex", alignItems: "center", color: "#999", fill: "currentColor", height: "32px", borderRadius: "4px", border: "1px solid #ddd", padding: "0 11px", marginRight: "10px"}} href={this.state.photo.links.download}>
+            {this.props.show_like_button && <a style={{display: "flex", alignItems: "center", color: "#999", fill: "currentColor", textDecoration: "none", height: "32px", borderRadius: "4px", border: "1px solid #ddd", padding: "0 11px", marginRight: "10px"}} href={this.state.photo.links.download}>
               <svg style={{marginRight: "6px"}} viewBox="0 0 32 32" width="15" height="32">
                 <path fill="#f15151" d="M17.4 29c-.8.8-2 .8-2.8 0l-12.3-12.8c-3.1-3.1-3.1-8.2 0-11.4 3.1-3.1 8.2-3.1 11.3 0l2.4 2.8 2.3-2.8c3.1-3.1 8.2-3.1 11.3 0 3.1 3.1 3.1 8.2 0 11.4l-12.2 12.8z" />
               </svg>
@@ -69,7 +69,7 @@ export class Embed extends React.Component<Props, State> {
               </svg>
             </a>}
           </div>
-          <p>
+          <p style={{margin: "0"}}>
             {this.props.show_location && this.state.photo.location && <React.Fragment><small style={{fontSize: "12px"}}>{this.state.photo.location.title}</small><br/></React.Fragment>}
             {this.props.show_description && <React.Fragment>{this.state.photo.description || "..."}<br/></React.Fragment>}
             {this.props.show_exif && <small style={{fontSize: "12px", color: "#999"}}>{this.state.photo.exif.model} {this.state.photo.exif.focal_length}mm f/{this.state.photo.exif.aperture} {this.state.photo.exif.exposure_time} {this.state.photo.exif.iso}ISO</small>}
